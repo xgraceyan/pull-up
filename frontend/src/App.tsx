2a5ea4f1399741284e65e13d377f31152b784f99
Grace Yan <xgraceyan@gmail.com>
2025-09-05 21:01:24 -0700

    Frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { WeekDayTimeCalendar } from "./components/calendar/WeekDayTimeCalendar/WeekDayTimeCalendar";
+
+async function fetchHello() {
+  return new Promise<string>((resolve) =>
+    setTimeout(() => resolve("TanStack Test"), 500)
+  );
+}
+
+export default function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["hello"],
+    queryFn: fetchHello,
+  });
+
+  return (
+    <div className="flex flex-col h-screen pt-10">
+      <p className="text-lg font-medium text-center">
+        {isLoading ? "Loading..." : data}
+      </p>
+      <div className="grid grid-cols-3 gap-4 text-center">
+        <div className="col-span-2 p-10">
+          {/* <CalendarRoot /> */}
+          <WeekDayTimeCalendar />
+        </div>
+        <div className="col-span-1">Hi</div>
+      </div>
+    </div>
+  );
+}
