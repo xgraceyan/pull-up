55044ebc0e80af150fc6f356ac4447c0e050625f
Grace Yan <xgraceyan@gmail.com>
2025-09-08 21:13:38 -0700

    Serialize objectIds to string in json

--- pull-up-backend/src/main/java/me/graceyan/pull_up_backend/config/JacksonConfig.java
+++ pull-up-backend/src/main/java/me/graceyan/pull_up_backend/config/JacksonConfig.java
@@ -1,7 +1,10 @@
 package me.graceyan.pull_up_backend.config;
 
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.bson.types.ObjectId;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +18,11 @@ public class JacksonConfig {
         return builder -> builder.simpleDateFormat("H:mm")
                 .serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern("H:mm")))
                 .deserializers(new LocalTimeDeserializer(DateTimeFormatter.ofPattern("H:mm")));
+    }
+    @Bean
+    public SimpleModule objectIdModule() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(ObjectId.class, ToStringSerializer.instance);
+        return module;
     }
 }
