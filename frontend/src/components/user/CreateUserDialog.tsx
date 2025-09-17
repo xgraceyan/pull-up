6f2bd90572245c1605cd10d13e2190d29070547d
Grace Yan <xgraceyan@gmail.com>
2025-09-16 18:20:27 -0700

    Refactor create user dialog to be react hook form

--- frontend/src/components/user/CreateUserDialog.tsx
+++ frontend/src/components/user/CreateUserDialog.tsx
@@ -15,37 +15,53 @@ import { useCreateUser } from "@/hooks/useUser";
 import type { Event } from "@/lib/event";
 import type { User, UserPayload } from "@/lib/user";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface CreateUserDialogProps {
   event: Event;
   setEditUser: (value: React.SetStateAction<User | null>) => void;
 }
 
+const UserFormSchema = z.object({
+  name: z.string().min(1, "Name is required."),
+  passwordRaw: z.string().optional(),
+});
+
+type UserFormData = z.infer<typeof UserFormSchema>;
+
 export const CreateUserDialog = ({
   event,
   setEditUser,
 }: CreateUserDialogProps) => {
-  const [formData, setFormData] = useState<UserPayload>({
-    name: "",
-    passwordRaw: "",
+  const form = useForm<UserFormData>({
+    resolver: zodResolver(UserFormSchema),
+    defaultValues: {
+      name: "",
+      passwordRaw: "",
+    },
   });
 
   const useCreateUserMutation = useCreateUser(event);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((data) => ({ ...data, [name]: value }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = (data: UserFormData) => {
     try {
-      useCreateUserMutation.mutate(formData, {
+      useCreateUserMutation.mutate(data as UserPayload, {
         onSuccess: (newUser) => {
           setEditUser(newUser);
+          form.reset();
         },
       });
-      setFormData({ name: "", passwordRaw: "" });
     } catch (error) {
       console.error(error);
     }
@@ -57,36 +73,56 @@ export const CreateUserDialog = ({
         <Button variant="outline">Create User</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <form onSubmit={handleSubmit}>
-          <DialogHeader>
-            <DialogTitle>Create User</DialogTitle>
-            <DialogDescription>
-              Add your availability to the calendar
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 mt-4 mb-6">
-            <div className="grid gap-3">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" name="name" onChange={handleChange} />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="passwordRaw">Password (optional)</Label>
-              <Input
-                id="passwordRaw"
+        <DialogHeader>
+          <DialogTitle>Create User</DialogTitle>
+          <DialogDescription>
+            Add your availability to the calendar
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <div className="grid gap-4 mt-4 mb-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
                 name="passwordRaw"
-                type="password"
-                placeholder="Leave blank for no password"
-                onChange={handleChange}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password (optional)</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Leave blank for no password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
             </div>
-          </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Confirm</Button>
-          </DialogFooter>
-        </form>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Confirm</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
